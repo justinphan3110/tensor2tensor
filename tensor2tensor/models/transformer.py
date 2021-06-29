22445cbc89191f1b85986b66e920e971f063a620
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-06-30 00:01:01 +0700

    add subset multicc

--- tensor2tensor/models/transformer.py
+++ tensor2tensor/models/transformer.py
@@ -41,12 +41,12 @@ from tensor2tensor.utils import mlperf_log
 from tensor2tensor.utils import registry
 from tensor2tensor.utils import t2t_model
 
+
+import tensorflow.compat.v1 as tf
 from tensor2tensor.utils.t2t_model import _create_target_modality, log_warn, log_info
 
 import collections
 import six
-
-import tensorflow.compat.v1 as tf
 
 # pylint: disable=g-direct-tensorflow-import
 from tensorflow.python.ops import inplace_ops
@@ -227,106 +227,6 @@ class Transformer(t2t_model.T2TModel):
         decode_loop_step=decode_loop_step, nonpadding=nonpadding, losses=losses,
         **kwargs)
   
-  def bottom(self, features):
-    """Transforms features to feed into body.
-
-    Args:
-      features: dict of str to Tensor. Typically it is the preprocessed data
-        batch after Problem's preprocess_example().
-
-    Returns:
-      transformed_features: dict of same key-value pairs as features. The value
-        Tensors are newly transformed.
-    """
-    if not self._problem_hparams:
-      log_warn("Without a Problem, T2TModel.bottom is a passthrough.")
-      return features
-
-    transformed_features = collections.OrderedDict()
-    all_previous_modalities = []
-    target_modality = _create_target_modality(self._problem_hparams.modality)
-
-    # Transform features via its corresponding modality.
-    for feature_name, modality in sorted(
-        six.iteritems(self._problem_hparams.modality)):
-      if feature_name not in features:
-        tf.logging.warning("Missing feature %s - ignoring." % feature_name)
-        continue
-      vocab_size = self._problem_hparams.vocab_size[feature_name]
-      if vocab_size is not None and hasattr(self._hparams, "vocab_divisor"):
-        vocab_size += (-vocab_size) % self._hparams.vocab_divisor
-      modality_name = self._hparams.name.get(
-          feature_name,
-          modalities.get_name(modality))(self._hparams, vocab_size)
-      # Use if-else clauses to preserve behavior of previous changes: namely,
-      # the variable scope name for the targets feature if there is only one
-      # target modality; and to reuse variable scopes for only input modalities.
-      if feature_name in target_modality:
-        if len(target_modality) > 1:
-          variable_scope_name = "%s/%s" % (modality_name, feature_name)
-        else:
-          variable_scope_name = modality_name
-        bottom = self._hparams.bottom.get(
-            feature_name,
-            modalities.get_targets_bottom(modality))
-        # TODO(aidangomez): share variables?
-        with tf.variable_scope(variable_scope_name) as vs:
-          self._add_variable_scope(variable_scope_name, vs)
-          log_info("Transforming feature '%s' with %s.targets_bottom",
-                   feature_name,
-                   modality_name)
-          transformed_features[feature_name] = bottom(features[feature_name],
-                                                      self._hparams,
-                                                      vocab_size)
-      else:
-        bottom = self._hparams.bottom.get(feature_name,
-                                          modalities.get_bottom(modality))
-        do_reuse = modality_name in all_previous_modalities
-        with tf.variable_scope(modality_name, reuse=do_reuse) as vs:
-          self._add_variable_scope(modality_name, vs)
-          log_info("Transforming feature '%s' with %s.bottom",
-                   feature_name,
-                   modality_name)
-          transformed_features[feature_name] = bottom(features[feature_name],
-                                                      self._hparams,
-                                                      vocab_size)
-        all_previous_modalities.append(modality_name)
-
-    inputs_tensor = features['inputs']
-    inputs_shape = inputs_tensor.shape
-    batch_size = inputs_shape[0]
-
-
-    num_special_tokens = batch_size
-    special_token_id = 21223
-
-    special_tokens = tf.constant([[ [[21223+x]] for x in range(0, num_special_tokens)]])
-    # special_tokens = tf.reshape(special_tokens, [batch_size, num_special_tokens, 1, 1])
-    # special_tokens = tf.repeat(special_tokens, batch_size, axis=1)
-    special_tokens = tf.repeat(special_tokens, batch_size, axis=0)
-    
-    print(special_tokens.shape)
-    inputs_tensor = tf.concat([special_tokens, inputs_tensor], 1)
-    features['inputs'] = inputs_tensor
-    
-    print("features", features) 
-    
-
-    for key in features:
-      if key not in transformed_features:
-        # For features without a modality, we pass them along as is
-        transformed_features[key] = features[key]
-      else:
-        # Other features get passed along with the "raw" suffix
-        transformed_features[key + "_raw"] = features[key]
-
-    transformed_features['inputs'] = tf.cast(inputs_tensor, dtype=tf.float32)
-
-    print("transformed_features", transformed_features)
-    return transformed_features
-
-
-
   def body(self, features):
     """Transformer main model_fn.
 
@@ -1872,6 +1772,114 @@ class TransformerMemory(Transformer):
     """
     return self._beam_decode_slow(features, decode_length, beam_size,
                                   top_beams, alpha, use_tpu)
+
+
+@registry.register_model
+class Transformerextratokens(Transformer):
+  """Attention net.  See file docstring."""
+
+  def __init__(self, *args, **kwargs):
+    super(Transformerextratokens, self).__init__(*args, **kwargs)
+
+  def bottom(self, features):
+    """Transforms features to feed into body.
+
+    Args:
+      features: dict of str to Tensor. Typically it is the preprocessed data
+        batch after Problem's preprocess_example().
+
+    Returns:
+      transformed_features: dict of same key-value pairs as features. The value
+        Tensors are newly transformed.
+    """
+    if not self._problem_hparams:
+      log_warn("Without a Problem, T2TModel.bottom is a passthrough.")
+      return features
+
+    hparams = self._hparams
+
+    transformed_features = collections.OrderedDict()
+    all_previous_modalities = []
+    target_modality = _create_target_modality(self._problem_hparams.modality)
+
+    # Transform features via its corresponding modality.
+    for feature_name, modality in sorted(
+        six.iteritems(self._problem_hparams.modality)):
+      if feature_name not in features:
+        tf.logging.warning("Missing feature %s - ignoring." % feature_name)
+        continue
+      vocab_size = self._problem_hparams.vocab_size[feature_name]
+      if vocab_size is not None and hasattr(self._hparams, "vocab_divisor"):
+        vocab_size += (-vocab_size) % self._hparams.vocab_divisor
+      modality_name = self._hparams.name.get(
+          feature_name,
+          modalities.get_name(modality))(self._hparams, vocab_size)
+      # Use if-else clauses to preserve behavior of previous changes: namely,
+      # the variable scope name for the targets feature if there is only one
+      # target modality; and to reuse variable scopes for only input modalities.
+      if feature_name in target_modality:
+        if len(target_modality) > 1:
+          variable_scope_name = "%s/%s" % (modality_name, feature_name)
+        else:
+          variable_scope_name = modality_name
+        bottom = self._hparams.bottom.get(
+            feature_name,
+            modalities.get_targets_bottom(modality))
+        # TODO(aidangomez): share variables?
+        with tf.variable_scope(variable_scope_name) as vs:
+          self._add_variable_scope(variable_scope_name, vs)
+          log_info("Transforming feature '%s' with %s.targets_bottom",
+                   feature_name,
+                   modality_name)
+          transformed_features[feature_name] = bottom(features[feature_name],
+                                                      self._hparams,
+                                                      vocab_size)
+      else:
+        bottom = self._hparams.bottom.get(feature_name,
+                                          modalities.get_bottom(modality))
+        do_reuse = modality_name in all_previous_modalities
+        with tf.variable_scope(modality_name, reuse=do_reuse) as vs:
+          self._add_variable_scope(modality_name, vs)
+          log_info("Transforming feature '%s' with %s.bottom",
+                   feature_name,
+                   modality_name)
+          transformed_features[feature_name] = bottom(features[feature_name],
+                                                      self._hparams,
+                                                      vocab_size)
+        all_previous_modalities.append(modality_name)
+
+    inputs_tensor = features['inputs']
+    inputs_shape = inputs_tensor.shape
+    batch_size = inputs_shape[0]
+
+    print('hparams ', hparams)
+    num_special_tokens = hparams['extra_tokens']
+    special_token_id = 21223
+
+    special_tokens = tf.constant([[ [[21223+x]] for x in range(0, num_special_tokens)]])
+    # special_tokens = tf.reshape(special_tokens, [batch_size, num_special_tokens, 1, 1])
+    # special_tokens = tf.repeat(special_tokens, batch_size, axis=1)
+    special_tokens = tf.repeat(special_tokens, batch_size, axis=0)
+    
+    print(special_tokens.shape)
+    inputs_tensor = tf.concat([special_tokens, inputs_tensor], 1)
+    features['inputs'] = inputs_tensor
+    
+    print("features", features) 
+    
+
+    for key in features:
+      if key not in transformed_features:
+        # For features without a modality, we pass them along as is
+        transformed_features[key] = features[key]
+      else:
+        # Other features get passed along with the "raw" suffix
+        transformed_features[key + "_raw"] = features[key]
+
+    transformed_features['inputs'] = tf.cast(inputs_tensor, dtype=tf.float32)
+
+    print("transformed_features", transformed_features)
+    return transformed_features
 
 
 @registry.register_hparams
