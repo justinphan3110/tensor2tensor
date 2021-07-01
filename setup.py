fe711dc22624f013430aae6ae7ffe908a44e0372
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-07-01 17:38:51 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     install_requires=[
         'absl-py',
         'bz2file',
-        'dopamine-rl==3.1.14',
+        'dopamine-rl==2.0.0',
         'flask',
         'future',
         'gevent',
