36937ae9c86711ea5f32d44450f4dfc0d22d39a7
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-07-01 17:19:46 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
         'tqdm',
     ],
     extras_require={
-        'tensorflow': ['tensorflow>=1.15.0'],
+        'tensorflow': ['tensorflow==1.15.5'],
         'tensorflow-hub': ['tensorflow-hub>=0.1.1'],
         'tests': [
             # Needed to fix a Travis pytest error.
