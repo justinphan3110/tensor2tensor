c9ed9b1a8e4aaf425467312c2053e591c86329f2
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-07-10 17:33:27 +0700

    add transformer tokens v2

--- tensor2tensor/models/transformer.py
+++ tensor2tensor/models/transformer.py
@@ -1890,6 +1890,29 @@ class Transformerextratokens(Transformer):
     return transformed_features
 
 
+
+@registry.register_model
+class TransformerExtraTokenV2(Transformer):
+  
+  def body(self, features):
+    
+    hparams = self._hparams
+    hidden_dim = hparams.hidden_size
+    num_extras = hparams.extra_tokens
+    
+    inputs = features['inputs']  # [batch_size, length, 1, hidden_dim]
+    batch_size = inputs.shape[0]
+    
+    extra_tokens = tf.get_variable(
+            'extra_tokens', [1, num_extras, 1, hidden_dim],
+            initializer=tf.random_normal_initializer(0.0, hidden_dim**-0.5)))
+    
+    extra_tokens = tf.repeat(extra_tokens, batch_size, axis=0)  # [batch, num_extras, 1, hidden_dim]
+    
+    features['inputs'] = tf.concat([extra_tokens, inputs], axis=1)  # [batch, num_extras+len, 1, hidden_dim]
+    
+    return super(TransformerExtraTokenV2, self).body(features)
+
 @registry.register_hparams
 def transformer_base_v1():
   """Set of hyperparameters."""
